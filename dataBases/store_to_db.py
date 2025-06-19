4b513aa85a11e4adc792ab5c40611e9e3fa5e095
Barakat <sedrabarakat@gmail.com>
2025-06-19 20:45:55 +0300

    text_procssing

--- dataBases/store_to_db.py
+++ dataBases/store_to_db.py
@@ -2,13 +2,12 @@ import sys
 import os
 import mysql.connector
 
-# اضيف جذر المشروع إلى sys.path 
+# أضف جذر المشروع إلى sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from corpus import get_corpus
-from app.text_processing.text_preprocessing import get_preprocessed_text_terms
+from text_processing.text_preprocessing import get_preprocessed_text_terms
 
-# إعداد الاتصال بقاعدة البيانات
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -21,18 +20,19 @@ datasets = ["antique", "beir"]
 
 for dataset_name in datasets:
     corpus = get_corpus(dataset_name)
-    first_doc = next(iter(corpus.items()))
-    doc_id, text = first_doc
+    items = list(corpus.items())
 
-    processed = get_preprocessed_text_terms(text, dataset_name)
-    processed_str = " ".join(processed)
+    quarter = len(items) // 4
+    three_quarters = 3 * quarter
 
-    # ✨ صار داخل اللوب
-    cursor.execute(
-        "INSERT INTO documents (document_id, dataset_name, processed_text, text) VALUES (%s, %s, %s, %s)",
-        (doc_id, dataset_name, processed_str, text)
-    )
+    for doc_id, text in items[three_quarters:]:
+        processed = get_preprocessed_text_terms(text, dataset_name)
+        processed_str = " ".join(processed)
 
+        cursor.execute(
+            "INSERT INTO documents (document_id, dataset_name, processed_text, text) VALUES (%s, %s, %s, %s)",
+            (doc_id, dataset_name, processed_str, text)
+        )
 
 conn.commit()
 cursor.close()
