22bf28d73f3108486773c480966df1ded317f0a0
Barakat <sedrabarakat@gmail.com>
2025-07-12 16:40:59 +0300

    update_tf

--- tfidf.py
+++ tfidf.py
@@ -1,4 +1,4 @@
-from text_processing.text_preprocessing import clean_and_tokenize_text
+from text_processing.text_preprocessing import get_preprocessed_text_terms
 
 def tokenizer(text):
-    return clean_and_tokenize_text(text, dataset_name="beir")
+    return get_preprocessed_text_terms(text, dataset_name="beir")
